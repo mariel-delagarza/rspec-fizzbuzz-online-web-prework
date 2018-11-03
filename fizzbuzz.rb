24802cfc8b8086931f6ab5613ad7d1375eed0a21
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-03 21:07:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,6 @@
 
 def fizzbuzz(x)
   if x % 3 == 0 
-    puts "Fizz"
-  elsif x % 5 == 0 
-    puts "Buzz"
-  elsif x % 15 == 0 
-    puts "FizzBuzz"
-  else 
-    puts nil 
+    "Fizz"
   end 
 end 
