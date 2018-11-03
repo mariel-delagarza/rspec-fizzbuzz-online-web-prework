781ceba13903cb64e42d1f7c4febe191a4650fc6
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-03 21:12:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(x)
-  if x % 3 && x % 5 == 0  
+def fizzbuzz(int)
+  if int % 3 && int % 5 == 0  
     "FizzBuzz"
   elsif  
-    x % 3 == 0 
+    int % 3 == 0 
     "Fizz"
-  elsif x % 5 == 0 
+  elsif int % 5 == 0 
     "Buzz"
   else 
     nil 
