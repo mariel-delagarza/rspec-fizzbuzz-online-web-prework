d0f5c01fa1c38950de232b75f6bb5b1e92b35eb3
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-03 21:05:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  
+def fizzbuzz(x)
+  if x%3 == 0 
+    puts "Fizz"
+  elsif x%5 == 0 
+    puts "Buzz"
+  elsif x%15 == 0 
+    puts "FizzBuzz"
+  else 
+    puts nil 
+  end 
 end 
